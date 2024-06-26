e9eff35da95b885a1a3ab30e2b9048f5657a35a1
erick8374 <erick.ldepaula@gmail.com>
2024-06-25 22:22:43 -0300

    Novo menu da interface do console e novo input test

--- entrada_dados.cpp
+++ entrada_dados.cpp
@@ -9,41 +9,56 @@
 void menu();
 int main() {
     std::locale::global(std::locale(""));
-    menu();
-    int op;
-    std::cin >> op;
-    switch (op)
-    {
-    case 1:
-        huffman_tree::compressFile();
-        // huffman_tree::showCodeTable();
-        //huffman_tree::showTree();
-        break;
-    case 2:
-        huffman_tree::descompressFile();
-        break;
+    int op = 0;
 
-    default:
-        break;
+    while (op != 5) {
+        menu();
+        std::cin >> op;
+        switch (op) {
+        case 1:
+            //huffman_tree::compressFile();
+            break;
+        case 2: {
+            auto path = huffman_tree::read_path();
+            auto caracter_count = huffman_tree::calculate_frequency(path);
+            auto root = huffman_tree::create_huffman(caracter_count);
+            huffman_tree::showTree(root);
+            break;
+        }
+        case 3:
+            //huffman_tree::showFrequenciesTable();
+            std::cout << "Em breve..." << std::endl;
+
+            break;
+        case 4: {
+            // auto path = huffman_tree::read_path();
+            // auto caracter_count = huffman_tree::calculate_frequency(path);
+            // auto root = huffman_tree::create_huffman(caracter_count);
+            // huffman_tree::descompressFile();
+            std::cout << "Em breve..." << std::endl;
+
+            break;
+        }
+        case 5:
+            std::cout << "Saindo..." << std::endl;
+            break;
+        default:
+            std::cout << "Opção inválida! Por favor, escolha novamente." << std::endl;
+            break;
+        }
     }
-    
-    // auto word_count =  huffman_tree::calculate_frequency("output.txt");
-    // for (const auto& entry : word_count) {
-    //     std::wcout << entry.first << L": " << entry.second << L'\n';
-
-    // }
-    // std::wcout << L" ------------------------------------\n";
-    // huffman_tree::compressFile();
-    // std::wcout << L" ------------------------------------\n";
 
     return 0;
 }
+
 void menu() {
     std::cout << "==================================" << std::endl;
     std::cout << "    Compactação de Arquivos" << std::endl;
     std::cout << "==================================" << std::endl;
     std::cout << "1. Compactar arquivo" << std::endl;
-    std::cout << "2. Descompactar arquivo" << std::endl;
-    std::cout << "3. Sair" << std::endl;
+    std::cout << "2. Mostrar árvore de Huffman" << std::endl;
+    std::cout << "3. Mostrar tabela de frequências e códigos de Huffman" << std::endl;
+    std::cout << "4. Descompactar arquivo" << std::endl;
+    std::cout << "5. Sair" << std::endl;
     std::cout << "Escolha uma opção: ";
 }
